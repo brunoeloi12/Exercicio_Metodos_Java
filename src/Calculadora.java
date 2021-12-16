f26cd80c5257655892e1e2552fac5d036dca4119
Bruno Eloi <brunoeloi12@gmail.com>
2021-12-16 10:39:47 -0300

    Primeiro commit

---
+++ src/Calculadora.java
@@ -0,0 +1,35 @@
+public class Calculadora {
+
+    public static void soma(double num1, double num2){
+
+        double resultado = num1 + num2;
+
+        System.out.println("A soma entre os números " + num1 + " e " + num2 + " é igual a: " + resultado );
+
+    }
+
+    public static void subtracao(double numb1, double numb2){
+
+        double resultado = numb1 - numb2;
+
+        System.out.println("A subtração entre os números " + numb1 + " e " + numb2 + " é igual a: " + resultado );
+
+    }
+
+    public static void divisao(double numb1, double numb2){
+
+        double resultado = numb1 / numb2;
+
+        System.out.println("A divisão entre os numeros " + numb1 + " e " + numb2 + " é igual a: " + resultado);
+
+    }
+
+    public static void mutiplicacao(double numb1, double numb2){
+
+        double resultado = numb1 * numb2;
+
+        System.out.println("A mutiplicação entre os números " + numb1 +  " e " + numb2 + " é igual a: " + resultado);
+
+    }
+
+}
